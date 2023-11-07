275a1303914342aa8d17d4dc038d546862b9cd2c
XiChen <chenxi171@mails.ucas.ac.cn>
2023-11-07 16:56:51 +0800

    Revert "Timing: split 4096*512 SRAM into multiple 1024*128"
    This reverts commit ce04554400dec0a9331ffc8ad9ebe641e3c753de.

--- src/main/scala/coupledL2/DataStorage.scala
+++ src/main/scala/coupledL2/DataStorage.scala
@@ -19,7 +19,7 @@ package coupledL2
 
 import chisel3._
 import chisel3.util._
-import coupledL2.utils.{BankedSRAM, SRAMTemplate}
+import coupledL2.utils.SRAMTemplate
 import utility.RegNextN
 import org.chipsalliance.cde.config.Parameters
 
@@ -49,30 +49,20 @@ class DataStorage(implicit p: Parameters) extends L2Module {
     val wdata = Input(new DSBlock)
   })
 
-  // convert SRAM size from 4096 * 512 to multiple 1024 * 128
-  val dataSplit = 4 // split a cache block
-  val indexSplit = 4 // split SRAM index
-  val dataSplitWidth = blockBytes * 8 / dataSplit
+  val array = Module(new SRAMTemplate(
+    gen = new DSBlock,
+    set = blocks,
+    way = 1,
+    singlePort = true
+  ))
 
   val arrayIdx = Cat(io.req.bits.way, io.req.bits.set)
   val wen = io.req.valid && io.req.bits.wen
   val ren = io.req.valid && !io.req.bits.wen
+  array.io.w.apply(wen, io.wdata, arrayIdx, 1.U)
+  array.io.r.apply(ren, arrayIdx)
 
-  val array = Seq.fill(dataSplit) {
-    Module(new BankedSRAM(
-      gen = UInt(dataSplitWidth.W),
-      sets = blocks,
-      ways = 1,
-      n = indexSplit
-    ))
-  }
-
-  for (i <- 0 until dataSplit) {
-    array(i).io.w.apply(wen, io.wdata.data(dataSplitWidth * (i + 1) - 1, dataSplitWidth * i), arrayIdx, 1.U)
-    array(i).io.r.apply(ren, arrayIdx)
-  }
-
-  io.rdata := RegNext(Cat(array.map(_.io.r.resp.data(0)).reverse).asTypeOf(new DSBlock))
   // TODO: timing: we should not use reg here, instead set this as multicycle path
   // s3 read, s4 pass and s5 to destination
+  io.rdata := RegNextN(array.io.r.resp.data(0), 1)
 }
